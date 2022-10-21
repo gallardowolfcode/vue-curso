931c10498133182a02dc7f5a3ec8aeedc0870b7c
gallardowolfcode <you@example.com>
2022-10-20 22:07:33 -0500

    clase miercoles

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,46 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <b-button
+      label="Presioname"
+      size="is-medium"
+      type="is-primary"
+      @click="snackbar"
+    />
+    <div class="columns m-5">
+      <div class="column is-1 notification is-dark">First column</div>
+      <div class="column is-3 notification is-info">Second column</div>
+      <div class="column is-2 notification is-success">Third column</div>
+      <div class="column notification">
+        <div class="columns">
+          <div class="column red title has-text-white">hola 1</div>
+          <div class="column yellow"><span class="title is-4">hola 2</span></div>
+          <div class="column red subtitle has-text-info">hola 3</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    snackbar() {
+      this.$buefy.snackbar.open({
+        message: "Hola soy el snackbar",
+        type: "is-info",
+        position: "is-bottom",
+        actionText: "Hola",
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.red {
+  background-color: red;
+}
+.yellow {
+  background-color: yellow;
+}
+</style>
